61d522db1b3e520e87576a3b570632f2ec86d323
Saeed <Saeed.ssdr@gmail.com>
2023-12-30 22:47:32 +0330

    trying new syntax
    Just trying new syntax

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string = "Ace of Spades"
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Heart")
+
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(i+1, cards[i])
+	}
+}
+
+func newCard() string {
+	return "Five of Dimonds"
 }
