efc8127d704701f4363794b6ae29964f5ca0469a
Saeed <Saeed.ssdr@gmail.com>
2024-01-01 01:53:41 +0330

    added newDeck function

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Heart")
+	cards := newDeck()
 
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
+	cards.print()
 
-	for i := 0; i < 3; i++ {
-		fmt.Println(i+1, cards[i])
-	}
-}
-
-func newCard() string {
-	return "Five of Dimonds"
 }
